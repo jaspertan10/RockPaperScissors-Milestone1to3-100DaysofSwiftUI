3d48f222044ec944d8e2630e7ca715ce617c6ffd
Jasper Tan <jatan@Jaspers-Mac-mini.local>
2024-11-11 21:01:48 -0800

    initial commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Jasper Tan on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
